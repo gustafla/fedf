723833b15d339c55c72a95f04663f53e530a0e94
Lauri Gustafsson <luutifa@gmail.com>
2014-10-15 23:34:56 +0300

    Better packaging script.

--- send.sh
+++ send.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 mkdir /mnt/nfs/cubie/ff/
+mkdir /mnt/nfs/cubie/ff/src/
 cp *.exe /mnt/nfs/cubie/ff/
 cp *.dll /mnt/nfs/cubie/ff/
 cp -r characters/ /mnt/nfs/cubie/ff/
 cp -r stages/ /mnt/nfs/cubie/ff/
 cp -r gfx/ /mnt/nfs/cubie/ff/
 cp -r music/ /mnt/nfs/cubie/ff/
+cp makefile /mnt/nfs/cubie/ff/src/
+cp *.cpp /mnt/nfs/cubie/ff/src/
+cp *.hpp /mnt/nfs/cubie/ff/src/
+cp *.txt /mnt/nfs/cubie/ff/src/
+cp *.sh /mnt/nfs/cubie/ff/src/
+cp COPYING /mnt/nfs/cubie/ff/
